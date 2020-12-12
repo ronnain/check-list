421037a21e38a3309882696a8922caa54bac768c
Romain <romain.geffrault@laposte.net>
2020-12-12 20:08:16 +0100

    first start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { SubHeaderComponent } from './sub-header/sub-header.component';
+import { GoalsComponent } from './goals/goals.component';
+import { CheckListComponent } from './check-list/check-list.component';
+import { NewHabitComponent } from './new-habit/new-habit.component';
+import { RewardsComponent } from './rewards/rewards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SubHeaderComponent,
+    GoalsComponent,
+    CheckListComponent,
+    NewHabitComponent,
+    RewardsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/new-habit/new-habit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-habit',
+  templateUrl: './new-habit.component.html',
+  styleUrls: ['./new-habit.component.scss']
+})
+export class NewHabitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
